3c421a31e4a7ad97a8a42218f1f9ef7384d0bd15
Fatikh930 <fatikhkhan2@gmail.com>
2023-11-15 11:01:10 +0500

    app and test_cases

---
+++ testfile.py
@@ -0,0 +1,16 @@
+import unittest
+from calculatorapp import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        calculator = Calculator()
+        result = calculator.add(2, 2)
+        self.assertEqual(result, 4)
+
+    def test_subtract(self):
+        calculator = Calculator()
+        result = calculator.subtract(5, 2)
+        self.assertEqual(result, 3)
+
+if __name__ == '__main__':
+    unittest.main()
